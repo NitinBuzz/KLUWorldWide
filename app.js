c7cc83a41d152e2a83dbc2fbb5bbeb217d9d80b2
NitinBuzz <reachnitinvenkat@gmail.com>
2017-10-03 21:10:41 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.get('/', (req, res) => {
+  res.render('<h1>Home Page</h1>');
+});
+
+app.get('/about', (req, res) => {
+  res.render('<h1>About Page</h1>');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is up on PORT ${PORT}`);
+});
